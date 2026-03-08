2e6350552d349bdd9dba77a2e01654a427b560ab
max <maxtclinton@gmail.com>
2026-03-08 13:28:10 -0400

    add serialization roundtrip tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,140 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Test.Hspec
+import Test.QuickCheck
+import Data.Serialize (encode, decode, runPut, runGet)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Word (Word64)
+
+import Haskoin.Types
+
 main :: IO ()
-main = putStrLn "Tests not yet implemented"
+main = hspec $ do
+  describe "VarInt" $ do
+    it "encodes single-byte values (< 0xfd)" $ do
+      encode (VarInt 0) `shouldBe` "\x00"
+      encode (VarInt 1) `shouldBe` "\x01"
+      encode (VarInt 252) `shouldBe` "\xfc"
+
+    it "encodes two-byte values (0xfd prefix)" $ do
+      let encoded = encode (VarInt 253)
+      BS.length encoded `shouldBe` 3
+      BS.head encoded `shouldBe` 0xfd
+
+    it "encodes four-byte values (0xfe prefix)" $ do
+      let encoded = encode (VarInt 0x10000)
+      BS.length encoded `shouldBe` 5
+      BS.head encoded `shouldBe` 0xfe
+
+    it "encodes eight-byte values (0xff prefix)" $ do
+      let encoded = encode (VarInt 0x100000000)
+      BS.length encoded `shouldBe` 9
+      BS.head encoded `shouldBe` 0xff
+
+    it "roundtrips all VarInt sizes" $ property $ \(n :: Word64) ->
+      decode (encode (VarInt n)) == Right (VarInt n)
+
+  describe "Hash256" $ do
+    it "serializes to exactly 32 bytes" $ do
+      let hash = Hash256 (BS.replicate 32 0xab)
+      BS.length (encode hash) `shouldBe` 32
+
+    it "roundtrips correctly" $ do
+      let hash = Hash256 (BS.replicate 32 0xcd)
+      decode (encode hash) `shouldBe` Right hash
+
+  describe "Hash160" $ do
+    it "serializes to exactly 20 bytes" $ do
+      let hash = Hash160 (BS.replicate 20 0xef)
+      BS.length (encode hash) `shouldBe` 20
+
+    it "roundtrips correctly" $ do
+      let hash = Hash160 (BS.replicate 20 0x12)
+      decode (encode hash) `shouldBe` Right hash
+
+  describe "OutPoint" $ do
+    it "serializes to 36 bytes (32 hash + 4 index)" $ do
+      let txid = TxId (Hash256 (BS.replicate 32 0x00))
+          op = OutPoint txid 0
+      BS.length (encode op) `shouldBe` 36
+
+    it "roundtrips correctly" $ do
+      let txid = TxId (Hash256 (BS.replicate 32 0xaa))
+          op = OutPoint txid 42
+      decode (encode op) `shouldBe` Right op
+
+  describe "TxIn" $ do
+    it "roundtrips correctly" $ do
+      let txid = TxId (Hash256 (BS.replicate 32 0xbb))
+          op = OutPoint txid 1
+          txin = TxIn op "script" 0xffffffff
+      decode (encode txin) `shouldBe` Right txin
+
+  describe "TxOut" $ do
+    it "roundtrips correctly" $ do
+      let txout = TxOut 50000000 "pubkeyscript"
+      decode (encode txout) `shouldBe` Right txout
+
+  describe "BlockHeader" $ do
+    it "serializes to exactly 80 bytes" $ do
+      let prevHash = BlockHash (Hash256 (BS.replicate 32 0x00))
+          merkle = Hash256 (BS.replicate 32 0x00)
+          header = BlockHeader 1 prevHash merkle 0 0 0
+      BS.length (encode header) `shouldBe` 80
+
+    it "roundtrips correctly" $ do
+      let prevHash = BlockHash (Hash256 (BS.replicate 32 0x11))
+          merkle = Hash256 (BS.replicate 32 0x22)
+          header = BlockHeader 2 prevHash merkle 1234567890 0x1d00ffff 12345
+      decode (encode header) `shouldBe` Right header
+
+  describe "Tx (legacy)" $ do
+    it "roundtrips a simple legacy transaction" $ do
+      let txid = TxId (Hash256 (BS.replicate 32 0x00))
+          txin = TxIn (OutPoint txid 0) "scriptsig" 0xffffffff
+          txout = TxOut 100000 "scriptpubkey"
+          tx = Tx 1 [txin] [txout] [[]] 0
+      decode (encode tx) `shouldBe` Right tx
+
+  describe "Tx (segwit)" $ do
+    it "roundtrips a segwit transaction" $ do
+      let txid = TxId (Hash256 (BS.replicate 32 0x00))
+          txin = TxIn (OutPoint txid 0) "" 0xffffffff
+          txout = TxOut 100000 "scriptpubkey"
+          witness = ["sig", "pubkey"]
+          tx = Tx 2 [txin] [txout] [witness] 0
+      decode (encode tx) `shouldBe` Right tx
+
+  describe "Block" $ do
+    it "roundtrips correctly" $ do
+      let prevHash = BlockHash (Hash256 (BS.replicate 32 0x00))
+          merkle = Hash256 (BS.replicate 32 0x00)
+          header = BlockHeader 1 prevHash merkle 0 0 0
+          txid = TxId (Hash256 (BS.replicate 32 0x00))
+          txin = TxIn (OutPoint txid 0xffffffff) "coinbase" 0xffffffff
+          txout = TxOut 5000000000 "scriptpubkey"
+          coinbase = Tx 1 [txin] [txout] [[]] 0
+          block = Block header [coinbase]
+      decode (encode block) `shouldBe` Right block
+
+  describe "NetworkAddress" $ do
+    it "serializes to 26 bytes (8 services + 16 address + 2 port)" $ do
+      let addr = NetworkAddress 1 (BS.replicate 16 0x00) 8333
+      BS.length (encode addr) `shouldBe` 26
+
+    it "roundtrips correctly" $ do
+      let addr = NetworkAddress 0x409 (BS.replicate 16 0xff) 8333
+      decode (encode addr) `shouldBe` Right addr
+
+  describe "VarString" $ do
+    it "roundtrips correctly" $ do
+      let vs = VarString "hello world"
+      decode (encode vs) `shouldBe` Right vs
+
+  describe "WitnessStack" $ do
+    it "roundtrips correctly" $ do
+      let ws = WitnessStack ["item1", "item2", "item3"]
+      decode (encode ws) `shouldBe` Right ws
