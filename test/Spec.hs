a186609d6a1b06a43b1070ce385b6f780a087271
max <maxtclinton@gmail.com>
2026-03-09 21:31:04 -0400

    add address encoding tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,7 @@ import Data.Serialize (encode, decode, runPut, runGet)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base16 as B16
+import qualified Data.Text as T
 import Data.Word (Word64)
 
 import Haskoin.Types
@@ -253,3 +254,128 @@ main = hspec $ do
           BlockHash (Hash256 computed) = computeBlockHash header
       -- Block hash should be 32 bytes
       BS.length computed `shouldBe` 32
+
+  -- Address encoding tests
+  describe "Base58Check" $ do
+    it "encodes P2PKH address correctly" $ do
+      -- Known test vector: hash160 -> address
+      -- Using zero hash for simplicity
+      let hash = Hash160 (BS.replicate 20 0x00)
+          addr = addressToText (PubKeyAddress hash)
+      -- Address should start with '1' for mainnet P2PKH
+      T.head addr `shouldBe` '1'
+
+    it "encodes P2SH address correctly" $ do
+      let hash = Hash160 (BS.replicate 20 0x00)
+          addr = addressToText (ScriptAddress hash)
+      -- P2SH addresses start with '3' on mainnet
+      T.head addr `shouldBe` '3'
+
+    it "roundtrips P2PKH address" $ do
+      let hash = Hash160 (fst $ B16.decode "89abcdefabbaabbaabbaabbaabbaabbaabbaabba")
+          addr = PubKeyAddress hash
+          encoded = addressToText addr
+      textToAddress encoded `shouldBe` Just addr
+
+    it "roundtrips P2SH address" $ do
+      let hash = Hash160 (fst $ B16.decode "0011223344556677889900aabbccddeeff001122")
+          addr = ScriptAddress hash
+          encoded = addressToText addr
+      textToAddress encoded `shouldBe` Just addr
+
+    it "decodes known P2PKH address" $ do
+      -- 1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2 is a well-known address
+      -- Its hash160 is: 77bff20c60e522dfaa3350c39b030a5d004e839a
+      let knownAddr = "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2"
+          expectedHash = fst $ B16.decode "77bff20c60e522dfaa3350c39b030a5d004e839a"
+      case textToAddress knownAddr of
+        Just (PubKeyAddress (Hash160 h)) -> h `shouldBe` expectedHash
+        _ -> expectationFailure "Failed to decode known address"
+
+    it "rejects invalid checksum" $ do
+      -- Modify last character to invalidate checksum
+      textToAddress "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN3" `shouldBe` Nothing
+
+  describe "Bech32" $ do
+    it "encodes P2WPKH address correctly" $ do
+      let hash = Hash160 (BS.replicate 20 0x00)
+          addr = addressToText (WitnessPubKeyAddress hash)
+      -- Should start with bc1q for mainnet P2WPKH
+      T.take 4 addr `shouldBe` "bc1q"
+
+    it "encodes P2WSH address correctly" $ do
+      let hash = Hash256 (BS.replicate 32 0x00)
+          addr = addressToText (WitnessScriptAddress hash)
+      -- Should start with bc1q for mainnet P2WSH (witness version 0)
+      T.take 4 addr `shouldBe` "bc1q"
+
+    it "roundtrips P2WPKH address" $ do
+      let hash = Hash160 (fst $ B16.decode "751e76e8199196d454941c45d1b3a323f1433bd6")
+          addr = WitnessPubKeyAddress hash
+          encoded = addressToText addr
+      textToAddress encoded `shouldBe` Just addr
+
+    it "roundtrips P2WSH address" $ do
+      let hash = Hash256 (fst $ B16.decode "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f")
+          addr = WitnessScriptAddress hash
+          encoded = addressToText addr
+      textToAddress encoded `shouldBe` Just addr
+
+    it "decodes known P2WPKH address" $ do
+      -- bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4 corresponds to
+      -- hash160: 751e76e8199196d454941c45d1b3a323f1433bd6
+      let knownAddr = "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4"
+          expectedHash = fst $ B16.decode "751e76e8199196d454941c45d1b3a323f1433bd6"
+      case textToAddress knownAddr of
+        Just (WitnessPubKeyAddress (Hash160 h)) -> h `shouldBe` expectedHash
+        _ -> expectationFailure "Failed to decode known bech32 address"
+
+  describe "Bech32m" $ do
+    it "encodes Taproot address correctly" $ do
+      let hash = Hash256 (BS.replicate 32 0x00)
+          addr = addressToText (TaprootAddress hash)
+      -- Should start with bc1p for mainnet P2TR
+      T.take 4 addr `shouldBe` "bc1p"
+
+    it "roundtrips Taproot address" $ do
+      let hash = Hash256 (fst $ B16.decode "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798")
+          addr = TaprootAddress hash
+          encoded = addressToText addr
+      textToAddress encoded `shouldBe` Just addr
+
+    it "decodes known Taproot address" $ do
+      -- bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0
+      -- This is a BIP-350 test vector
+      let knownAddr = "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0"
+      case textToAddress knownAddr of
+        Just (TaprootAddress _) -> return ()  -- Success, just check it parses as Taproot
+        _ -> expectationFailure "Failed to decode known taproot address"
+
+  describe "Address construction" $ do
+    it "pubKeyToP2PKH creates P2PKH address" $ do
+      let pk = PubKeyCompressed (BS.cons 0x02 (BS.replicate 32 0xab))
+          addr = pubKeyToP2PKH pk
+      case addr of
+        PubKeyAddress _ -> return ()
+        _ -> expectationFailure "Expected P2PKH address"
+
+    it "pubKeyToP2WPKH creates P2WPKH address" $ do
+      let pk = PubKeyCompressed (BS.cons 0x02 (BS.replicate 32 0xab))
+          addr = pubKeyToP2WPKH pk
+      case addr of
+        WitnessPubKeyAddress _ -> return ()
+        _ -> expectationFailure "Expected P2WPKH address"
+
+    it "scriptToP2SH creates P2SH address" $ do
+      let script = BS.pack [0x00, 0x14] <> BS.replicate 20 0xcd
+          addr = scriptToP2SH script
+      case addr of
+        ScriptAddress _ -> return ()
+        _ -> expectationFailure "Expected P2SH address"
+
+    it "scriptToP2WSH creates P2WSH address" $ do
+      let script = BS.pack [0x52, 0x21] <> BS.replicate 33 0xab <> BS.pack [0x21] <> BS.replicate 33 0xcd <> BS.pack [0x52, 0xae]
+          addr = scriptToP2WSH script
+      case addr of
+        WitnessScriptAddress _ -> return ()
+        _ -> expectationFailure "Expected P2WSH address"
